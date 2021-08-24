a034fc68c2ae8fb8e6699a35d04fd5f0b6de3325
Harikrishnan S <70973180+harikrishnanssankar@users.noreply.github.com>
2021-08-25 00:43:52 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import DisplayCount from "./Count";
 import Counter from "./Counter";
+import { getUser } from "./redux/ducks/user";
 import "./styles.css";
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUser());
+  }, [dispatch]);
+
+  const user = useSelector((state) => state.user.user);
+  console.log(user);
+
   const contestants = ["person 1", "person 2", "person 3", "person 4"];
 
   return (
@@ -17,8 +29,15 @@ export default function App() {
     >
       <DisplayCount />
       {contestants.map((contestant) => {
-        return <Counter name={contestant} />;
+        return (
+          <div key={contestant}>
+            <Counter name={contestant} />
+          </div>
+        );
       })}
+      <div style={{ position: "absolute", bottom: "10px", right: "10px" }}>
+        <h2>Created By: {user?.name}</h2>
+      </div>
     </div>
   );
 }
--- src/Count.js
+++ src/Count.js
@@ -4,7 +4,7 @@ const DisplayCount = () => {
   const count = useSelector((state) => state.counter.count);
   return (
     <div style={{ padding: "30px" }}>
-      <h1 style={{ paddingBottom: "10px" }}>Fans</h1>
+      <h1 style={{ paddingBottom: "10px" }}>Fan Fight</h1>
       <h2>Total votes: {count}</h2>
     </div>
   );
